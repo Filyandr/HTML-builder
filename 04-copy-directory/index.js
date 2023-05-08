ba4c7745e00eba18cfc7d13e0c55a81bfb2fb7ae
Filyandr <philippkotelevets@gmail.com>
2023-05-08 23:06:13 +0400

    feat: add solution to task #4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const path = require('path');
+const fs = require('fs')
+const fsPromises = require('fs/promises');
+const { stdout } = process;
 
+const pathFolder = path.resolve(__dirname, 'files');
+const pathCopyFolder = path.resolve(__dirname, 'files-copy');
+
+async function copyDirectory() {
+  try {
+    await fsPromises.rm(pathCopyFolder, { recursive: true, force: true });
+    await fsPromises.mkdir(pathCopyFolder, { recursive: true });
+
+    const files = await fsPromises.readdir(pathFolder, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const filePath = path.resolve(pathFolder, file.name);
+        const fileCopyPath = path.resolve(pathCopyFolder, file.name);
+
+        fs.copyFile(filePath, fileCopyPath, error => {
+          if (error) throw error;
+        });
+      }}
+  } catch (error) {
+    console.log(`Can't copy directory: ${error}`);
+  }
+}
+copyDirectory();
