a17560f98c22dae7128f4135bcdf94a5f58b9355
Filyandr <philippkotelevets@gmail.com>
2023-05-08 17:49:38 +0400

    feat: resolve 2-nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin, stdout } = process;
+
+const filePath = path.resolve(__dirname, 'text.txt');
+
+stdout.write('Bonjour! \nВведите текст, он добавится в "text.txt"\n');
+
+stdin.on("data", (data) => {
+    if (data.toString().trim().toLowerCase() === 'exit') {
+        stdout.write('Счастливо!');
+        process.exit();
+    } else {
+    fs.appendFile((filePath), data, (err) => {
+        if (err) throw err;
+    });
+}
+});
+
+process.on('SIGINT', () => {
+    stdout.write('Счастливо!');
+    process.exit();
+})
